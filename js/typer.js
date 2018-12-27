9867b8331e145173c9f46406dbb58490736c5726
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-12-27 16:55:23 +0530

    Lets Do something Different.
    Change-Id: Ibc1764f1bb6ff9df5487e213c47e2d7de887bcc5
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- js/typer.js
+++ js/typer.js
@@ -10,7 +10,7 @@ var Typer = function(element) {
   this.progress = { word:0, char:0, building:true, atWordEnd:false, looped: 0 };
   this.typing = true;
 
-  var colors = element.dataset.colors || "white";
+  var colors = element.dataset.colors || "black";
   this.colors = colors.split(",");
   this.element.style.color = this.colors[0];
   this.colorIndex = 0;
