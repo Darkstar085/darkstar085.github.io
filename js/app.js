bbce69bef5481bac86708184368e109004c8b20e
S I P U N <sipunkumar85@gmail.com>
2024-03-12 19:56:51 +0530

    Particle effect changes
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,54 +1,62 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 100, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    number: {
+      value: 500,
+      density: {
+        enable: false,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#fff",
+    },
     shape: {
       type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 3 },
-      image: { src: "img/github.svg", width: 100, height: 100 },
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
     },
     opacity: {
       value: 1,
       random: true,
-      anim: { enable: true, speed: 1, opacity_min: 0.1, sync: true },
+      anim: {
+        enable: false,
+        speed: 0.31907341081499313,
+        opacity_min: 0.015953670540749657,
+        sync: true,
+      },
     },
     size: {
-      value: 3,
+      value: 6,
       random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
     },
     line_linked: {
       enable: false,
-      distance: 150,
+      distance: 500,
       color: "#ffffff",
       opacity: 0.4,
-      width: 1,
+      width: 2,
     },
     move: {
       enable: true,
-      speed: 2,
+      speed: 3.1508467900748314,
       direction: "none",
       random: false,
       straight: false,
-      out_mode: "out",
+      out_mode: "bounce",
       bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 },
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "remove" },
-      resize: true,
-    },
-    modes: {
-      grab: { distance: 200, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
+      attract: {
+        enable: true,
+        rotateX: 6144.151240645921,
+        rotateY: 6065.380070894051,
+      },
     },
   },
   retina_detect: true,
